7f4bf511f83eb02490fd4a79ccfcf166c075e070
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2026-04-20 07:01:41 +0000

    Remove some comments

--- testProglinkedStack-1.cpp
+++ testProglinkedStack-1.cpp
@@ -1,55 +1,49 @@
 //Program to test the various operations on an
 //ordered linked list
 
-#include <iostream>
-#include "derivedLinkedStack.h"
+#include <iostream>                             //Line 1
+#include "derivedLinkedStack.h"                  //Line 2
 
-using namespace std;
+using namespace std;                            //Line 3
 
-int main()
-{
-	linkedStackType<int> stack1, stack2;
-	int num;
+int main()                                      //Line 4
+{                                               //Line 5
+	linkedStackType<int> stack1, stack2;        //Line 6
+	int num;                                    //Line 7
 
-	// Keep reading values until the sentinel shows up.
 	cout << "Line 8: Enter numbers ending "
-		 << "with -999." << endl;
-	cin >> num;
+			<< "with -999." << endl;               //Line 8
+	cin >> num;                                 //Line 9
 
-	while (num != -999)
-	{
-		cout << "Pushing: " << num << endl;
-		stack1.push(num);
-		cin >> num;
-	}
+	while (num != -999)                         //Line 10
+	{                                           //Line 11
+			cout << "Pushing: " << num << endl;
+			stack1.push(num);                      //Line 12
+			cin >> num;                             //Line 13
+	}                                           //Line 14
 
-	cout << endl;
+	cout << endl;                               //Line 15
 
-	// Show what ended up in the first stack.
-	cout << "Line 16: stack1: ";
-	stack1.print();
-	cout << endl;
+	cout << "Line 16: stack1: ";                 //Line 16
+	stack1.print();                              //Line 17
+	cout << endl;                               //Line 18
 
-	// Make a copy so we can prove the stacks are independent.
 	stack2 = stack1; //test the assignment operator Line 19
 
-	cout << "Line 20: stack2: ";
-	stack2.print();
-	cout << endl;
+	cout << "Line 20: stack2: ";                 //Line 20
+	stack2.print();                              //Line 21
+	cout << endl;                               //Line 22
 
-	// Remove the top item from the copy only.
-	cout << "Line 23: popping the top node " << stack2.top() << endl;
+	cout << "Line 23: popping the top node" << stack2.top() << endl;
 
-	stack2.pop();
+	stack2.pop();                      //Line 26
 
-	// Print both stacks again to confirm the original stayed the same.
 	cout << "Line 27: After Popping "
-		 << num << ", stack2: " << endl;
-	stack2.print();
-	cout << endl;
+			<< num << ", stack2: " << endl;         //Line 27
+	stack2.print();                              //Line 28
+	cout << endl;                               //Line 29
 	cout << "Prove that list1 was untouched" << endl;
 	stack1.print();
 
-	return 0;
-}
- 
+	return 0;                                   //Line 30
+}   
